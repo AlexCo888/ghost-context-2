8f429244102621f0eb1c0aae77e5426861926d67
Alexco888 <contacto@cafedelossentidos.com>
2023-05-14 12:33:16 -0600

    First Commit

---
+++ src/app/components/EnsInput.js
@@ -0,0 +1,46 @@
+import { useEnsAddress } from 'wagmi';
+import { useState } from 'react';
+
+export default function EnsInput() {
+  const [ensName, setEnsName] = useState('');
+  const { data, isError, isLoading } = useEnsAddress({
+    name: ensName,
+    chainId: 1,
+  });
+
+  const handleSubmit = async (e, ensName) => {
+    e.preventDefault();
+    return ensName;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='items-center'>
+          <label
+            htmlFor='ens'
+            className='block text-sm font-medium leading-6 text-white px-5 py-3'
+          >
+            or with Ens
+          </label>
+          <input
+            type='text'
+            value={ensName}
+            name='ens'
+            id='ens'
+            onChange={(e) => setEnsName(e.target.value)}
+            className='block w-30 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            placeholder='vitalik.eth'
+          />
+        </div>
+      </form>
+      {isLoading ? (
+        <div>Fetching address…</div>
+      ) : isError ? (
+        <div>Error fetching address</div>
+      ) : (
+        <div>Address: {data}</div>
+      )}
+    </div>
+  );
+}
