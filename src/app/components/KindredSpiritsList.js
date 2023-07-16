d21fa8400efafb24ce4e4c6111f6144ae7ac627f
Alexco888 <contacto@cafedelossentidos.com>
2023-07-16 17:30:28 -0600

    Change .csv length

--- src/app/components/KindredSpiritsList.js
+++ src/app/components/KindredSpiritsList.js
@@ -100,7 +100,7 @@ const KindredSpiritsList = () => {
   
   async function downloadKindredCSV() {
     setButtonText("Downloading...");
-    const dataPromises = Object.entries(filteredContractsForModal).slice(0, 50).map(async ([address, contract]) => {
+    const dataPromises = Object.entries(filteredContractsForModal).slice(0, 20).map(async ([address, contract]) => {
         const count = contract.count || 0;
         const contractsInCommon = contract.contractsInCommon || [];
 
